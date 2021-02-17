47e970a7cdc28d51828a60d7ae2d7efe17d8b78f
Ahmed <abumeis1991@gmail;COM>
2021-02-17 14:13:06 +0100

    hotel et hotel card

--- trippy/src/components/HotelCard .jsx
+++ trippy/src/components/HotelCard .jsx
@@ -1,17 +1,19 @@
 import React from "react";
 // import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 class HotelCard extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>HotelCard</h1>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <img src={this.props.pictures} alt="" />
+        <p>{this.props.rating}</p>
+        <p>{this.props.price}</p>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
 }
 
 export default HotelCard;
