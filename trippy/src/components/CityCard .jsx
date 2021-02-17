6d55e363103a550ca5bb1f0e7584f6c3fbfd929e
Schenkel <thi.schenkel@gmail.com>
2021-02-17 16:09:26 +0100

    css2

--- trippy/src/components/CityCard .jsx
+++ trippy/src/components/CityCard .jsx
@@ -4,23 +4,25 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 class CityCard extends React.Component {
   render() {
     const src = "http://localhost:3002" + this.props.src;
+    console.log(this.props.src);
     return (
       <BrowserRouter>
 
         <Link to={`/Hotels/${this.props.slug}`} style={{ textDecoration: "none", color: "#767676", fontSize: "16px" }}>
           <div className="container">
             <div className="row">
-              <div className="col-sm-4 col-md-6" >
-                <img src="http://localhost:3002/img/paris.png" class="card-img-top" alt={this.props.slug["Paris"]} />
-                <p class="card-text" ><span>{this.props.name["Paris"]}</span></p>
-              </div>
-              <div className="col-sm-2 col-md-4" >
-                <img src={src} class="card-img-top" alt={this.props.slug} />
-                <p class="card-text" ><span>{this.props.name}</span></p>
-              </div>
+              {(this.props.src === "/img/paris.png") ?
+                (<div className="col-sm-4 col-md-6" >
+                  <img src="http://localhost:3002/img/paris.png" class="card-img-top" alt={this.props.slug} />
+                  <p class="card-text" ><span>{this.props.name}</span></p>
+                </div>) :
+                (<div className="col-sm-2 col-md-4" >
+                  <img src={src} class="card-img-top" alt={this.props.slug} />
+                  <p class="card-text" ><span>{this.props.name}</span></p>
+                </div>)
+              }
             </div>
           </div>
-
         </Link>
 
       </BrowserRouter>
