37de97b17cda58f384c0b272c9b33104de2d2cbb
Schenkel <thi.schenkel@gmail.com>
2021-02-17 17:56:51 +0100

    Css ajouté

--- trippy/src/components/CityCard.jsx
+++ trippy/src/components/CityCard.jsx
@@ -6,26 +6,21 @@ class CityCard extends React.Component {
     const src = "http://localhost:3002" + this.props.src;
     console.log(this.props.src);
     return (
-      <BrowserRouter>
 
-        <Link to={`/Hotels/${this.props.slug}`} style={{ textDecoration: "none", color: "#767676", fontSize: "16px" }}>
-          <div className="container">
-            <div className="row">
-              {(this.props.src === "/img/paris.png") ?
-                (<div className="col" >
-                  <img src="http://localhost:3002/img/paris.png" class="card-img-top" alt={this.props.slug} />
-                  <p class="card-text" ><span>{this.props.name}</span></p>
-                </div>) :
-                (<div className="col-sm-4 col-md-4" >
-                  <img src={src} class="card-img-top" alt={this.props.slug} />
-                  <p class="card-text" ><span>{this.props.name}</span></p>
-                </div>)
-              }
-            </div>
-          </div>
-        </Link>
-
-      </BrowserRouter>
+      <div className="container">
+        <div className="row">
+          {(this.props.src === "/img/paris.png") ?
+            (<div className="col" >
+              <img src="http://localhost:3002/img/paris.png" class="card-img-top" alt={this.props.slug} />
+              <p class="card-text" ><span>{this.props.name}</span></p>
+            </div>) :
+            (<div className="col-sm-4 col-md-4" >
+              <img src={src} class="card-img-top" alt={this.props.slug} />
+              <p class="card-text" ><span>{this.props.name}</span></p>
+            </div>)
+          }
+        </div>
+      </div>
     );
   }
 }
--- trippy/src/views/Home.jsx
+++ trippy/src/views/Home.jsx
@@ -27,8 +27,7 @@ class Home extends React.Component {
       <div>
         {this.state.cities.map((ville) => {
           return (
-
-            <Link to={"/Hotels/" + ville.slug}>
+            <Link to={"/Hotels/" + ville.slug} style={{ textDecoration: "none", color: "#767676", fontSize: "16px" }}>
               <CityCard
                 src={ville.source}
                 slug={ville.slug}
